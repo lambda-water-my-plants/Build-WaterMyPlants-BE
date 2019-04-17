cc96318fa8ce08dd5ecd0c4760ce7fddbc980403
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-04-17 16:50:38 -0400

    testing

--- data/seeds/003_watering.js
+++ data/seeds/003_watering.js
@@ -5,8 +5,8 @@ exports.seed = async function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('watering').insert([
-        { plant_id: 1, watering_time: '2019-04-17 8:00' },
-        { plant_id: 2, watering_time: '2019-04-17 14:00' },
+        { plant_id: 1, watering_time: '2019-04-18 8:00' },
+        { plant_id: 2, watering_time: '2019-04-19 14:00' },
       ]);
     });
 };
